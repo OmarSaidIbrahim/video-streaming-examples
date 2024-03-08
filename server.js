909771cf24bd4774e56c8053d7bef4494cee7ff1
Omar Said Ibrahim <omarsaidita@gmail.com>
2024-03-09 00:36:11 +0100

    fix server

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ let videoQuality = "sd";
 let latency;
 
 app.use(responseTime());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async function (req, res) {
   let videoId = req?.query?.videoId;
@@ -37,7 +36,7 @@ app.get("/", async function (req, res) {
       )?.link;
     }
   }
-  res.sendFile(__dirname + "public/index.html");
+  res.sendFile(__dirname + "index.html");
 });
 
 app.get("/getVideoDetails", async function (req, res) {
